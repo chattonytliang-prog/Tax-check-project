3bb0eaae6259d81cf48f991cb578558b24571c9c
Tony <Chattonytliang@gmail.com>
2026-07-03 19:23:34 +0800

    Recognize prepaid balance import aliases

--- src/lib/clientImportParser.test.ts
+++ src/lib/clientImportParser.test.ts
@@ -21,8 +21,8 @@ describe('clientImportParser', () => {
 
   it('parses template-style CSV headers into client fields', () => {
     const parsed = parseClientImportText([
-      '企业名称,统一社会信用代码,月收入,月成本费用,销项税额,银行收款流水,本月开票金额,未开票收入,连续零申报,近12个月销售额,电商平台收入,私户收款,红字发票金额,职工人数,外包人数,参保人数,个税申报人数,应付职工薪酬,个人劳务费',
-      '上海模板测试有限公司,91310000TEMPLATE,100000,60000,13000,280000,96000,是,否,1180000,420000,是,18000,36,8,32,35,310000,42000',
+      '企业名称,统一社会信用代码,月收入,月成本费用,销项税额,银行收款流水,本月开票金额,未开票收入,连续零申报,预收账款长期挂账,近12个月销售额,电商平台收入,私户收款,红字发票金额,职工人数,外包人数,参保人数,个税申报人数,应付职工薪酬,个人劳务费',
+      '上海模板测试有限公司,91310000TEMPLATE,100000,60000,13000,280000,96000,是,否,是,1180000,420000,是,18000,36,8,32,35,310000,42000',
     ].join('\n'))
 
     expect(parsed.patch).toMatchObject({
@@ -35,6 +35,7 @@ describe('clientImportParser', () => {
       monthlyInvoice: '96000',
       unbilledIncome: '是',
       longTermZeroDeclaration: '否',
+      prepaidLongTerm: '是',
       consecutive12MonthSales: '1180000',
       platformRevenue: '420000',
       privateAccountCollection: '是',
@@ -54,6 +55,7 @@ describe('clientImportParser', () => {
       'monthlyInvoice',
       'unbilledIncome',
       'longTermZeroDeclaration',
+      'prepaidLongTerm',
       'consecutive12MonthSales',
       'platformRevenue',
       'privateAccountCollection',
