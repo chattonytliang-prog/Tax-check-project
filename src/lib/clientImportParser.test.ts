ebeb1777548052f368fa688d1e22665e959e6b95
Tony <Chattonytliang@gmail.com>
2026-07-03 18:23:33 +0800

    Recognize outsourced labor count aliases

--- src/lib/clientImportParser.test.ts
+++ src/lib/clientImportParser.test.ts
@@ -21,8 +21,8 @@ describe('clientImportParser', () => {
 
   it('parses template-style CSV headers into client fields', () => {
     const parsed = parseClientImportText([
-      '企业名称,统一社会信用代码,月收入,月成本费用,销项税额,银行收款流水,本月开票金额,近12个月销售额,电商平台收入,私户收款,红字发票金额,职工人数,参保人数,个税申报人数,应付职工薪酬,个人劳务费',
-      '上海模板测试有限公司,91310000TEMPLATE,100000,60000,13000,280000,96000,1180000,420000,是,18000,36,32,35,310000,42000',
+      '企业名称,统一社会信用代码,月收入,月成本费用,销项税额,银行收款流水,本月开票金额,近12个月销售额,电商平台收入,私户收款,红字发票金额,职工人数,外包人数,参保人数,个税申报人数,应付职工薪酬,个人劳务费',
+      '上海模板测试有限公司,91310000TEMPLATE,100000,60000,13000,280000,96000,1180000,420000,是,18000,36,8,32,35,310000,42000',
     ].join('\n'))
 
     expect(parsed.patch).toMatchObject({
@@ -38,6 +38,7 @@ describe('clientImportParser', () => {
       privateAccountCollection: '是',
       redVatSpecialInvoiceAmount: '18000',
       employees: '36',
+      laborCount: '8',
       socialSecurityCount: '32',
       salaryDeclaredCount: '35',
       payrollTotal: '310000',
@@ -54,6 +55,7 @@ describe('clientImportParser', () => {
       'privateAccountCollection',
       'redVatSpecialInvoiceAmount',
       'employees',
+      'laborCount',
       'socialSecurityCount',
       'salaryDeclaredCount',
       'payrollTotal',
