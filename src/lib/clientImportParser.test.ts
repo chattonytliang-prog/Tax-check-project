7be0cc5feef7af967ae1a1d52f5396da4458b5c6
Tony <Chattonytliang@gmail.com>
2026-07-03 19:03:28 +0800

    Recognize zero declaration import aliases

--- src/lib/clientImportParser.test.ts
+++ src/lib/clientImportParser.test.ts
@@ -21,8 +21,8 @@ describe('clientImportParser', () => {
 
   it('parses template-style CSV headers into client fields', () => {
     const parsed = parseClientImportText([
-      '企业名称,统一社会信用代码,月收入,月成本费用,销项税额,银行收款流水,本月开票金额,未开票收入,近12个月销售额,电商平台收入,私户收款,红字发票金额,职工人数,外包人数,参保人数,个税申报人数,应付职工薪酬,个人劳务费',
-      '上海模板测试有限公司,91310000TEMPLATE,100000,60000,13000,280000,96000,是,1180000,420000,是,18000,36,8,32,35,310000,42000',
+      '企业名称,统一社会信用代码,月收入,月成本费用,销项税额,银行收款流水,本月开票金额,未开票收入,连续零申报,近12个月销售额,电商平台收入,私户收款,红字发票金额,职工人数,外包人数,参保人数,个税申报人数,应付职工薪酬,个人劳务费',
+      '上海模板测试有限公司,91310000TEMPLATE,100000,60000,13000,280000,96000,是,否,1180000,420000,是,18000,36,8,32,35,310000,42000',
     ].join('\n'))
 
     expect(parsed.patch).toMatchObject({
@@ -34,6 +34,7 @@ describe('clientImportParser', () => {
       collectionFlow: '280000',
       monthlyInvoice: '96000',
       unbilledIncome: '是',
+      longTermZeroDeclaration: '否',
       consecutive12MonthSales: '1180000',
       platformRevenue: '420000',
       privateAccountCollection: '是',
@@ -52,6 +53,7 @@ describe('clientImportParser', () => {
       'collectionFlow',
       'monthlyInvoice',
       'unbilledIncome',
+      'longTermZeroDeclaration',
       'consecutive12MonthSales',
       'platformRevenue',
       'privateAccountCollection',
